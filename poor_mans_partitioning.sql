f4a6adeccfea927c38a49901a39a4a2beb8c7904
Ken Kuhlman <github@redlagoon.net>
2018-08-16 10:42:31 -0500

    Update poor_mans_partitioning.sql

--- poor_mans_partitioning.sql
+++ poor_mans_partitioning.sql
@@ -4,75 +4,106 @@
 --
 -- A successful partitioning scheme will avoid lookups against tables (partitions) that aren't being 
 --   "needed" by the query.
--- So to highlight any faults in the approaches, we'll leave off the index on the first partition 
---  on the data column.  It'll just have keys on the partitioning fields.
+-- An earlier iteration of this script left off indexes on one of the partitions to attempt to highlight this.
+--  But extra highlighting shouldn't be necessary, and may not even be possible (given MTIs), so now things are uniform.
 
 -- Create tables 
 -----------------
 execute immediate('set path =' || current_schema); 
+
+drop index jan01; 
+drop index jan02;
+drop index feb01; 
+drop index feb02;
+
+--repeated this until we have 8 million records. (proc below creates 1m
+insert into jan (mo, dtanbr, dtachr) select mo, dtanbr, dtachr from jan order by rid;
+insert into feb (mo, dtanbr, dtachr) select mo, dtanbr, dtachr from feb order by rid;
+commit;    
+
 begin
   declare exit handler for sqlstate '42710' /* already exists, sql0601 */ 
   begin
-    set gv_rebuild_tables = 'N';
-  end;
-  execute immediate('create variable gv_rebuild_tables char(1) default(''N'')');
+    execute immediate('set gv_rebuild_tables = ''N''');
+  end;  
+  execute immediate('create variable gv_rebuild_tables char(1) default(''Y'')');
 end;
 
 values gv_rebuild_tables; 
+stop; 
 
 begin
-   declare create_rows integer default 100000;
-   declare cur_row integer;
+  if gv_rebuild_tables = 'Y' then
+    drop table jan;
+    drop table feb;
+  end if;
+end;
+
+begin  /* build tables if needed */
+  declare create_rows integer default 1000000;
+  declare cur_row integer;
+  
+  set current schema kxk632;
+  set path kxk632;  
 
   if gv_rebuild_tables = 'Y' then
     /* generate test data */
-    drop table jan;
-    drop table feb;
-    create table jan (
+    
+    begin 
+      declare continue handler for sqlstate'42704' /* SQL0204 */  begin end;
+      drop index jan01;
+      drop index jan02;
+      drop index feb01;
+      drop index feb02;
+    end;
+
+    create or replace table jan (
       rid bigint generated always as identity primary key, 
       mo smallint not null, 
       dtanbr smallint not null, 
       dtachr char(10) not null,
       chgtsp timestamp not null generated always for each row on update as row change timestamp
       );
-    create table feb like jan including identity column attributes including row change timestamp column attributes;
-    --alter table feb add primary key (rid); 
+    create or replace table feb like jan including identity column attributes including row change timestamp column attributes;
     
     set cur_row = 1;
       while (cur_row <= create_rows) do
-        insert into jan (mo, dtanbr, dtachr) values(01, int(rand() * 255), TRANSLATE(CHAR(BIGINT(RAND() * 10000000000)), 'kenkuhlman', '1234567890'));
-        insert into feb (mo, dtanbr, dtachr) values(02, int(rand() * 255), TRANSLATE(CHAR(BIGINT(RAND() * 10000000000)), 'kenkuhlman', '1234567890'));
+        insert into jan (mo, dtanbr, dtachr) values(01, int(rand() * 255), left(TRANSLATE(CHAR(BIGINT(RAND() * 10000000000)), 'kenkuhlman', '1234567890'),10));
+        insert into feb (mo, dtanbr, dtachr) values(02, int(rand() * 255), left(TRANSLATE(CHAR(BIGINT(RAND() * 10000000000)), 'kenkuhlman', '1234567890'),10));
         set cur_row = cur_row + 1;
       end while;
-    end if;
-    
-    create index jan01 on jan (mo); 
-    -- No index on data for one of the months so we can see tblscan if partitioning logic is not getting optimized.
-    --create index jan02 on jan (dta);   
-    create index feb01 on feb (mo); 
-    create index feb02 on feb (dta); 
+  end if;  
 end; 
 commit;    
 
+begin
+    declare continue handler for sqlstate '42710' /* SQL0601
+ */  begin end;
+    create index jan01 on jan (mo); 
+    create index jan02 on jan (dtanbr);
+    create index feb01 on feb (mo); 
+    create index feb02 on feb (dtanbr);
+end; 
+
 
 create or replace view months_data_only as (
-select dta from jan
+select dtanbr, dtachr from jan
  union all
-select dta from feb
+select dtanbr, dtachr from feb
 );
 
 create or replace view months_data_and_month_nbr as
- (select mo, dta from jan
+ (select mo, dtanbr, dtachr from jan
   union all
-  select mo, dta from feb
+  select mo, dtanbr, dtachr from feb
 );
 
 -- this is identical to above, but won't be used until after we alter the tables to have constraints.
 --  having a distinct name to query makes it easier to check costs by query
 create or replace view months_data_and_month_nbr_constrained as
- (select mo, dta from jan
+ (select mo, dtanbr, dtachr from jan
   union all
-  select mo, dta from feb
+  select mo, dtanbr, dtachr from feb
 );
 
 -- Adding a derived field to the view is logically equivalent to having the field in the table..
@@ -87,16 +118,22 @@ create or replace view months_data_month_nbr_and_forced_month_name as
 
 -- Run queries once to get stats requests in & buffer whatever we can't avoid buffering when we purge purging pools later.
 -------------------------------------------------------------
-select count(*) from months_data_only where dta = 5;
-select count(*) from months_data_and_month_nbr where mo = 2 and dta = 5;
-select count(*) from months_data_and_month_nbr_constrained where mo = 2 and dta = 5;
-select count(*) from months_data_month_nbr_and_forced_month_name where month_name = 'FEBRUARY' and mo = 2 and dta = 5;
--- Wait a bit to let system auto-gather stats.  Wish I could runstats here.
-call qsys2.qcmdexc('dlyjob 120'); 
+select count(*) from months_data_only where dtanbr = 5;
+select count(*) from months_data_and_month_nbr where mo = 2 and dtanbr = 5;
+select count(*) from months_data_and_month_nbr_constrained where mo = 2 and dtanbr = 5;
+select count(*) from months_data_month_nbr_and_forced_month_name where month_name = 'FEBRUARY' and mo = 2 and dtanbr = 5;
+-- Wait a bit to let system auto-gather stats.  Wish I could do a runstats here to force them to create.
+--call qsys2.qcmdexc('dlyjob 120'); 
 
 
 call qsys2.qcmdexc('SETOBJACC OBJ('||current_schema||'/JAN) OBJTYPE(*FILE) POOL(*PURGE)');
 call qsys2.qcmdexc('SETOBJACC OBJ('||current_schema||'/FEB) OBJTYPE(*FILE) POOL(*PURGE)');
+
+release mydb;
+disconnect mydb; 
+values x;
+connect to mydb; 
+
 
 ---------------------------------
 CALL QSYS2.SET_MONITOR_OPTION(1);
@@ -104,44 +141,45 @@ CALL QSYS2.SET_MONITOR_OPTION(1);
 CALL QSYS2.QCMDEXC('QSYS/STRDBG UPDPROD(*YES)');
 CALL QSYS2.OVERRIDE_QAQQINI(2, 'OPEN_CURSOR_THRESHOLD', '-1');
 
-CALL qsys2.qcmdexc('STRDBMON OUTFILE(qtemp/tmpmonx) JOB(*) TYPE(*DETAIL)  COMMENT(DONT_REGISTER_MONITOR)');
+--CALL qsys2.qcmdexc('STRDBMON OUTFILE(dbmon/tmpmon) JOB(*) TYPE(*DETAIL)  COMMENT(DONT_REGISTER_MONITOR)');
+CALL qsys2.qcmdexc('STRDBMON OUTFILE(dbmon/tmpmon) JOB(*) TYPE(*DETAIL)');
+commit;
 
-begin 
- --TODO add continue handler here.  42704   SQL0204
+begin /* clean up constraints from previous runs */
+  declare continue handler for sqlstate '42704' /* SQL0204 */  begin end;
   alter table jan drop constraint only_jan;
   alter table feb drop constraint only_feb;
 end; 
 
-begin
+begin  /* run perf tests without constraints */
    declare rtv_rows integer default 10000;
    declare cur_row integer;
    
-   declare rnddta smallint; 
+   declare cur_dta smallint;    
    declare junk integer;
       
-   set cur_row = 1;
+   set cur_row = 0;      
    while (cur_row <= rtv_rows) do
-     set rnddta = int(rand() * 255);
-     select count(*) into junk from months_data_only where dta = rnddta;         
+     set cur_dta = mod(cur_row, 255);
+     select count(*) into junk from months_data_only where dtanbr = cur_dta;
      set cur_row = cur_row + 1;
-   end while;
+   end while; 
    
-   set cur_row = 1;
+   set cur_row = 0;
    while (cur_row <= rtv_rows) do
-     set rnddta = int(rand() * 255);
-     select count(*) into junk from months_data_and_month_nbr where mo = 2 and dta = rnddta;         
+     set cur_dta = mod(cur_row, 255);
+     select count(*) into junk from months_data_and_month_nbr where mo = 2 and dtanbr = cur_dta;
      set cur_row = cur_row + 1;
    end while;
 
-   set cur_row = 1;
+   set cur_row = 0;
    while (cur_row <= rtv_rows) do
-     set rnddta = int(rand() * 255);
-     select count(*) into junk from months_data_month_nbr_and_forced_month_name where month_name = 'FEBRUARY' and mo = 2 and dta = rnddta;
+     set cur_dta = mod(cur_row, 255);
+     select count(*) into junk from months_data_month_nbr_and_forced_month_name where month_name = 'FEBRUARY' and mo = 2 and dtanbr = cur_dta;
      set cur_row = cur_row + 1;
    end while;
-   
 end; 
-stop; 
+
 
 commit;
 call qsys2.qcmdexc('ALCOBJ OBJ(('||current_schema||'/JAN *FILE *EXCL)) WAIT(5) CONFLICT(*RQSRLS)'); 
@@ -155,7 +193,6 @@ commit;rollback;
 
 alter table jan add constraint only_jan check (mo = 1);
 alter table feb add constraint only_feb check (mo = 2);
- stop; 
    
 -- Run again with check constraint enabled. 
 -------------------------------------------
@@ -163,32 +200,30 @@ begin
    declare rtv_rows integer default 10000;
    declare cur_row integer;
    
-   declare rnddta smallint; 
+   declare cur_dta smallint; 
    declare junk integer;
    
    set cur_row = 1;
    while (cur_row <= rtv_rows) do
-     set rnddta = int(rand() * 255);
-       select count(*) into junk from months_data_and_month_nbr_constrained where mo = 2 and dta = rnddta;
+     set cur_dta = mod(cur_row, 255);
+     select count(*) into junk from months_data_and_month_nbr_constrained where mo = 2 and dtanbr = cur_dta;
      set cur_row = cur_row + 1;
    end while;
 end; 
-   
 commit;    
-
 
 -- commit;  call qsys2.qcmdexc('RCLRSC *'); call qsys2.qcmdexc('dlyjob 120'); 
 
-
-
 CALL QSYS2.SET_MONITOR_OPTION(3);
-CALL qsys2.qcmdexc('ENDDBMON JOB(*) COMMENT(DONT_REGISTER_MONITOR)');
+--CALL qsys2.qcmdexc('ENDDBMON JOB(*) COMMENT(DONT_REGISTER_MONITOR)');
+CALL qsys2.qcmdexc('ENDDBMON JOB(*)');
 CALL QSYS2.QCMDEXC('QSYS/ENDDBG');
+
 
 
 drop table qtemp.performance_list_explainable; 
 create table qtemp.PERFORMANCE_LIST_EXPLAINABLE as (
-  WITH F AS (SELECT QQJFLD FROM qtemp.tmpmonx F
+  WITH F AS (SELECT QQJFLD FROM dbmon.tmpmon F
                WHERE F.QQRID = 1000 AND QQ1000L IS NOT NULL AND QQUCNT > 0 AND (QVC1C = 'Y' OR QQI8 >= 0)), 
   R AS (SELECT QQJFLD, QQUCNT, QVRCNT AS REFRESH_COUNT,
     CASE
@@ -206,7 +241,7 @@ create table qtemp.PERFORMANCE_LIST_EXPLAINABLE as (
             THEN 1
             ELSE 0
         END AS REAL_QUERY, QQSTIM, QVC102, QQJOB, QQUSER, QQJNUM, QQSYS, QQRDBN, QQUDEF
-            FROM qtemp.tmpmonx X
+            FROM dbmon.tmpmon X
             WHERE X.QQRID = 1000 AND QQJFLD IN (SELECT QQJFLD
                                                   FROM F) AND X.QQSTIM IS NOT NULL AND X.QQETIM IS NOT NULL AND X.QQ1000 IS NOT NULL AND (X.QVC1C =
                                                   'Y' OR X.QQC21 IN('DC','OP','FE','CL','HC', 'UP','DL','IN','SI','SK','SV','VI',
@@ -233,7 +268,7 @@ create table qtemp.PERFORMANCE_LIST_EXPLAINABLE as (
               END) AS ASYNCHRONOUS_DB_WRITES, SUM(CASE WHEN X.QQRID = 3019 THEN DECIMAL(X.QQi7, 19, 6) ELSE NULL END) AS ROWS_RETURNED_3019,
               SUM(CASE WHEN X.QQRID = 3019 THEN DECIMAL(X.QVP151, 19, 6) ELSE NULL END) AS PAGE_FAULTS, SUM(CASE WHEN X.QQRID = 3019 THEN
               DECIMAL(X.QQI8, 19, 6) ELSE NULL END) AS CALLS_TO_RETRIEVE_ROWS, MAX(CASE WHEN X.QQRID = 3014 THEN QQC83 ELSE NULL END) AS QRO_HASH
-             FROM qtemp.tmpmonx X
+             FROM dbmon.tmpmon X
              WHERE X.QQRID IN (3019,3020,3015,3014) AND (QQJFLD,QVRCNT) IN (SELECT QQJFLD,REFRESH_COUNT
                                                   FROM TT)
              GROUP BY X.QQJFLD)
@@ -260,17 +295,13 @@ create table qtemp.PERFORMANCE_LIST_EXPLAINABLE as (
   WITH NC
 ) with data;
 
-select * from qtemp.performance_list_explainable;
-create table perfexp41 as (select * from qtemp.performance_list_explainable) with data;
-insert into perfexp41 (select * from qtemp.performance_list_explainable);
-select * from perfexp41 order by QQI6_TOTAL desc;
-commit;
-stop; 
+create table performance_list_explainable_16million as (select * from qtemp.performance_list_explainable) with data; 
+-- alter table performance_list_explain
 
-2018-07-30 14:33:46.049728	2.432680	2305.192736	9147	0.252016	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_ONLY WHERE DTA = : H : H  
-2018-07-30 15:23:47.834871	0.177768	445.336192	9999	0.044538	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_MONTH_NBR_AND_FORCED_MONTH_NAME WHERE MONTH_NAME = 'FEBRUARY' AND MO = 2 AND DTA = : H : H  
-2018-07-30 15:16:56.923916	0.197384	402.879576	9999	0.040291	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR WHERE MO = 2 AND DTA = : H : H  
-2018-07-30 18:52:21.629920	0.444984	241.747160	9999	0.024177	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR_CONSTRAINED WHERE MO = 2 AND DTA = : H : H  
+select * from qtemp.performance_list_explainable order by qqi6_total desc;
+
+
+stop; 
 
 
 --select * from qtemp.performance_list_explainable;
@@ -334,7 +365,7 @@ WITH TT
         FROM qtemp.tmpmonx A)
   SELECT TT.*
     FROM TT
-    WHERE QQRID = 3018 OR (QQSYS = 'DPITEST ' AND QQJOB = 'QZDASOINIT' AND QQUSER = 'QUSER     ' AND QQJNUM = '400223' AND QQUCNT = 294 AND (QQRID
+    WHERE QQRID = 3018 OR (QQJOB = 'QZDASOINIT' AND QQUSER = 'QUSER     ' AND QQJNUM = '400223' AND QQUCNT = 294 AND (QQRID
       = 3004 OR QQRID = 3007 OR QQRID = 3010 OR QQRID = 3023 OR QQRID = 3014 OR QQRID = 3015 OR QQRID = 5002 OR QQRID = 3019 OR QQRID = 1000) AND
       QVRCNT =
       (SELECT MAX(QVRCNT)
@@ -369,3 +400,25 @@ BEGIN
 END;
 
 --values timeit('select count(*) from sysibm.sysdummy1');
+
+
+-- 8 million rows:
+-- 308.146448	9999	0.030817	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR_CONSTRAINED WHERE MO = 2 AND DTANBR = : H : H  
+-- 291.363208	10000	0.029136	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR WHERE MO = 2 AND DTANBR = : H : H  
+-- 282.014120	10000	0.028201	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_MONTH_NBR_AND_FORCED_MONTH_NAME WHERE MONTH_NAME = 'FEBRUARY' AND MO = 2 AND DTANBR = : H : H  
+-- 241.584952	10000	0.024158	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_ONLY WHERE DTANBR = : H : H  
+
+-- 16 million rows 
+--595.864552	10000	0.059586	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_MONTH_NBR_AND_FORCED_MONTH_NAME WHERE MONTH_NAME = 'FEBRUARY' AND MO = 2 AND DTANBR = : H : H
+--590.270872	10000	0.059027	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR WHERE MO = 2 AND DTANBR = : H : H
+--579.876168	9999	0.057993	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR_CONSTRAINED WHERE MO = 2 AND DTANBR = : H : H
+--400.902056	10000	0.040090	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_ONLY WHERE DTANBR = : H : H
+
+-- 32 million rows 
+-- 1125.710576	10000	0.112571	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR WHERE MO = 2 AND DTANBR = : H : H  
+-- 1016.307464	10000	0.101630	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_MONTH_NBR_AND_FORCED_MONTH_NAME WHERE MONTH_NAME = 'FEBRUARY' AND MO = 2 AND DTANBR = : H : H  
+-- 1010.622904	9999	0.101072	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_AND_MONTH_NBR_CONSTRAINED WHERE MO = 2 AND DTANBR = : H : H  
+-- 718.970904	10000	0.071897	SELECT COUNT ( * ) INTO : H : H FROM MONTHS_DATA_ONLY WHERE DTANBR = : H : H  
+
+
+select count(*) from jan;
