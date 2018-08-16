dc501b845ced23731aa7203ee9296748c242b3f0
Ken Kuhlman <github@redlagoon.net>
2018-08-16 13:50:56 -0500

    Update csisum.sql

--- csisum.sql
+++ csisum.sql
@@ -1,7 +1,5 @@
 call dbmon.set_csi_collection();
 
-drop table csisum; 
-create table csisum as (
 select mi.intnum, mi.datetime, sys.smscpucpu CPU_pct, sys.smscpuqpu CPU_sql, mi.smmtcpup cpu_pct_inter, 
   mi2.smmtcpup cpu_pct_batch, decimal(round(SMOARSPT / 1000.0,2),5,2) IRT, 
   decimal(round(SMOMRSPT / 1000.0,2),7,2) max_irt, mi.smmnum job_count_inter, mi2.smmnum job_count_batch,  
@@ -21,12 +19,43 @@ join qtemp.QAPMSMPOL pol
   on mi.intnum = pol.intnum
 join qtemp.QAPMSMSYS sys
   on mi.intnum = sys.intnum
-order by mi.intnum asc
-) with data
+order by 1 desc 
 ;
 
-select * from csisum where hour(datetime) between 11 and 16 order by 1; 
+-- FIXME: Merge this into above 
+-- Our monitoring threshhold for TLBIE time is 4.0
+create or replace alias qtemp.qapmsysint for qmpgdata.qapmsysint (Q197000105);
 
+SELECT INTNUM, DATETIME, decimal(round(SIDATA02 / double(512)/ SIDATA01,2),13,2) "Average TLBIE time micro" 
+FROM qtemp.qapmsysint WHERE SITYPE = 3 and SIDID = 1 -- and hour(datetime) = 12
+order by 2 desc
+;         
+
+-- FIXME: any other values to include above from system summary? 
+-------------
+--Seize Count 
+--Seize/Wait Time
+--Highest User Pool Paging
+--Number of full opens system wide
+--False traps space address    
+--Forced stream file pages
+--Total dispatch time
+--Wait state task CPU time (ms)      
+--CPU thread event wait time microseconds    
+--Unscaled SQL CPU microseconds 
+
+SELECT INTNUM, DTETIM, SYSIZC, SYSEZC, SYSZWT, SHCPU, SMUPL, SMXDU,
+  SMMMT, SMME, SYFOPN, SYHFTS, SYHFTH, SYSDTET, SYSDPFD, SYBTAPP,    
+  SYIFUS, SYIFTE, SYIFTA, SYDPCH, SYPTHV, SYPTINT, SYPTWS, SYPTWAIT, 
+  SYSQLCPU, SYPSLPU, SYTRUNIC, SYTRUNVTB, SYTITUIC, SYTFRMIC 
+FROM qtemp.qapmsystem
+ORDER BY 1 desc                                                    
+;
+
+
+
+-- What are our most common IRT values during peak hours?
+create or replace view qtemp.csisumirt as (
 select irt, count(*) intervals_with_irt, 
   hour(min(datetime))||':'||right(digits(minute(min(datetime))),2) first_occurance, 
   hour(max(datetime))||':'||right(digits(minute(max(datetime))),2) last_occurance,
@@ -40,7 +69,7 @@ select irt, count(*) intervals_with_irt,
   avg(fault_rate_user_pool) fault_rate_user_pool, 
   avg(fault_rate_max) fault_rate_max, avg(temp_storage_gb) temp_storage_gb, 
   decimal(round(avg(spoolf_rate),2),7,2) spoolf_rate
-from csisum c
+from qtemp.csisum c
 where hour(datetime) between 11 and 16
 group by irt
-order by 1;
+);
