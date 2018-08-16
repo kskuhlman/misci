b9af174ac3c33cdf4036832cf10eb812d305985a
Ken Kuhlman <github@redlagoon.net>
2018-08-16 12:09:38 -0500

    Create plan_cache_analysis_data.sql

---
+++ plan_cache_analysis_data.sql
@@ -0,0 +1,9 @@
+-- private data. ignore this file in git.
+insert into dbmon.staticlibs values('QSYS');
+insert into dbmon.staticlibs values('QSYS2');
+insert into dbmon.staticlibs values('QTEMP');
+insert into dbmon.staticlibs values('SESSION');
+insert into dbmon.staticlibs values('SYSIBM');
+insert into dbmon.staticlibs values('QUSRSYS');
+insert into dbmon.staticlibs values('QUSRBRM');
+commit;
