76182283903ceb02e32626706511c1a877bc1ebd
Ken Kuhlman <github@redlagoon.net>
2018-08-16 11:32:39 -0500

    Create long_runners_data.sql

---
+++ long_runners_data.sql
@@ -0,0 +1,5 @@
+insert into ip_client (profile, ip_address, allowed_query_min, hard_limit) values('BADUSER1','192.168.0.11', 5, 'Y');
+insert into ip_client (profile, ip_address, allowed_query_min, hard_limit) values('BADUSER1','192.168.0.11', 5, 'Y');
+insert into ip_client values('SOSOUSER', '192.168.0.101', 20, default); 
+
+commit;
